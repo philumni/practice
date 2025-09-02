db7a150c239668c14a843d0651e3b81f67839f2f
philumni <philumni@gmail.com>
2025-09-02 14:06:45 -0700

    Update Exercise.cs
    Updating

--- Practice/Exercise.cs
+++ Practice/Exercise.cs
@@ -9,19 +9,20 @@ public class Exercise
     public static void Main(string[] args)
     {
         DoCharsInCommon();
+        DoFizzBuzz();
 
     }
 
     public static void DoCharsInCommon()
     {
         Console.WriteLine("Give us a first string.");
-        string str1=Console.ReadLine() ?? "";
+        string str1 = Console.ReadLine() ?? "";
 
         Console.WriteLine("Give us a second string.");
-        string str2=Console.ReadLine() ?? "";
+        string str2 = Console.ReadLine() ?? "";
 
         Console.WriteLine("The characters in common are: ");
-        Console.WriteLine(CharsInCommon(str1, str2)); 
+        Console.WriteLine(CharsInCommon(str1, str2));
         Console.ReadLine();
     }
 
@@ -32,7 +33,7 @@ public class Exercise
         HashSet<char> secondString = new HashSet<char>();
 
         if (str1.Length == 0 || str2.Length == 0) throw new ArgumentException("String(s) cannot be empty");
-        
+
         foreach (char c in str2.ToCharArray())
         {
             secondString.Add(c);
@@ -43,17 +44,51 @@ public class Exercise
             if (secondString.Contains(c))
             {
                 charsInCommon.Add(c);
-            }   
+            }
         }
 
         return new String(charsInCommon.ToArray());
     }
 
+    public static void DoFizzBuzz()
+    {
+        Console.WriteLine("Give us a number for FizzBuzz inclusive.");
+        int n = Convert.ToInt32(Console.ReadLine());
 
- 
+        Console.WriteLine(DoFizzBuzzString(DoFizzBuzzMain(n)));
+        Console.ReadLine();
+    }
 
+    public static string DoFizzBuzzString(string[] result)
+    {
+        int j = 0;
 
+        String output = "";
+        for (; j < result.Length - 1; j++)
+        {
+            output += result[j] += ", ";
+        }
+        output += result[j];
+        return output;
+    }
 
+    private static string[] DoFizzBuzzMain(int n)
+    {
+        // accomodate zero with n + 1 , FizzBuzz(300) should show you "300."
+        string[] result = new string[n + 1];
 
+        // we can easily omit zero, if we wanted to.
+        for (int j = 0; j <= n; j++)
+        {
+            string key = "";
+            if (j % 3 == 0 && j % 5 == 0) key = "FizzBuzz";
+            else if (j % 3 == 0) key = "Fizz";
+            else if (j % 5 == 0) key = "Buzz";
+            else key = "" + j;
+            result[j] = key;
+        }
+        return result;
+    }   
+    
 
 }
