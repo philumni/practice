86136892f3acd747d6864881d96f95524010bd2d
philumni <philumni@gmail.com>
2025-10-07 11:47:46 -0700

    Added input checking and clarified the numbers in common text.

--- Practice/Exercise.cs
+++ Practice/Exercise.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 
 namespace Practice;
@@ -17,6 +18,8 @@ public class Exercise
     {
         Console.WriteLine("Enter a string to check if it's a palindrome:");
         string input = Console.ReadLine() ?? "";
+        if (input.Length == 0) doExit();
+
         bool result = isPalindrome(input);
         Console.WriteLine(result ? "It is a palindrome." : "It is not a palindrome.");
         Console.ReadLine();
@@ -26,18 +29,38 @@ public class Exercise
     {
         Console.WriteLine("Give us a first string.");
         string str1 = Console.ReadLine() ?? "";
+        if (str1.Length == 0) doExit();
 
         Console.WriteLine("Give us a second string.");
         string str2 = Console.ReadLine() ?? "";
+        if (str2.Length == 0) doExit();
 
-        Console.WriteLine("The characters in common are: ");
+        Console.WriteLine("The unique characters in common are: ");
         Console.WriteLine(CharsInCommon(str1, str2));
+        Console.ReadLine();
+    }
+    
+    public static void DoFizzBuzz()
+    {
+        Console.WriteLine("Give us a number (0 to 3000) for FizzBuzz inclusive.");
+ 
+        String num = Console.ReadLine() ?? "";
+
+        int n = 0;
+        if (num.Length == 0 || !Int32.TryParse(num, out n)) doExit();
+       
+        if (n < 0 || n > 3000) doExit();
+        
+        Console.WriteLine(DoFizzBuzzString(DoFizzBuzzMain(n)));
         Console.ReadLine();
     }
 
 
     public static string CharsInCommon(string str1, string str2)
     {
+        if (str1 == null || str2 == null) throw new ArgumentException("String(s) cannot be null");
+        if (str1.Length == 0 || str2.Length == 0) throw new ArgumentException("String(s) cannot be empty");
+
         HashSet<char> charsInCommon = new HashSet<char>();
         HashSet<char> secondString = new HashSet<char>();
 
@@ -59,14 +82,6 @@ public class Exercise
         return new String(charsInCommon.ToArray());
     }
 
-    public static void DoFizzBuzz()
-    {
-        Console.WriteLine("Give us a number for FizzBuzz inclusive.");
-        int n = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine(DoFizzBuzzString(DoFizzBuzzMain(n)));
-        Console.ReadLine();
-    }
 
     public static string DoFizzBuzzString(string[] result)
     {
@@ -83,6 +98,8 @@ public class Exercise
 
     private static string[] DoFizzBuzzMain(int n)
     {
+        if (n < 0 || n > 3000) throw new ArgumentException("Number must be between 0 and 3000 inclusive.");
+
         // accomodate zero with n + 1 , FizzBuzz(300) should show you "300."
         string[] result = new string[n + 1];
 
@@ -105,14 +122,25 @@ public class Exercise
 
     private static bool innerPalindrome(int leftIndex, int rightIndex, string s)
     {
+        if (s == null) throw new ArgumentException("String cannot be null");
+        if (s.Length == 0) throw new ArgumentException("String cannot be empty");
+
         if (s[leftIndex] != s[rightIndex]) return false;
 
-        // covers odd case, even case, and empty string
+        // covers odd string lenghth case, and even case.
         if (leftIndex >= rightIndex) return true;
 
         leftIndex++;
         rightIndex--;
         return innerPalindrome(leftIndex, rightIndex, s);
+    }
+
+    private static void doExit()
+    {
+        Console.WriteLine("Exiting due to invalid input. Regards.");
+        Console.WriteLine("Press Enter to exit.");
+        Console.ReadLine();
+        Environment.Exit(0);
     }
 
 }
